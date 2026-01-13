061f6cda7482a975c7642239cb214d0b3f8d3f16
Omkar Pathak <omkarpathak27@gmail.com>
2026-01-13 16:36:48 +0530

    Added sentry for monitoring

--- finance_tracker/settings.py
+++ finance_tracker/settings.py
@@ -18,6 +18,22 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
+import sentry_sdk
+
+if os.environ.get('SENTRY_DSN'):
+    sentry_sdk.init(
+        dsn=os.environ.get('SENTRY_DSN'),
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for tracing.
+        traces_sample_rate=1.0,
+        _experiments={
+            # Set continuous_profiling_auto_start to True
+            # to automatically start the profiler on when
+            # possible.
+            "continuous_profiling_auto_start": True,
+        },
+    )
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
