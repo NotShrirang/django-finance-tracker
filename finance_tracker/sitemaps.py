dfde191f853b98e7bcf336504c6f0fb19f062ff8
Omkar Pathak <omkarpathak27@gmail.com>
2026-01-14 17:29:08 +0530

    Added dynamic sitemap.xml generation for better SEO

---
+++ finance_tracker/sitemaps.py
@@ -0,0 +1,13 @@
+from django.contrib import sitemaps
+from django.urls import reverse
+
+class StaticViewSitemap(sitemaps.Sitemap):
+    priority = 0.5
+    changefreq = 'monthly'
+    protocol = 'https'
+
+    def items(self):
+        return ['landing', 'signup', 'account_login', 'contact', 'privacy-policy', 'terms-of-service', 'demo_login']
+
+    def location(self, item):
+        return reverse(item)
--- finance_tracker/urls.py
+++ finance_tracker/urls.py
@@ -20,12 +20,23 @@ from django.urls import path, include
 from django.views.generic.base import RedirectView
 from django.conf import settings
 
+from django.contrib.sitemaps.views import sitemap
+from blog.sitemap import BlogSitemap
+from finance_tracker.sitemaps import StaticViewSitemap
+
+sitemaps = {
+    'static': StaticViewSitemap,
+    'blog': BlogSitemap,
+}
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/login/', RedirectView.as_view(pattern_name='account_login', permanent=True)), # Redirect legacy login
     path('accounts/', include('allauth.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('favicon.ico', RedirectView.as_view(url='/static/img/pwa-icon-512.png')),
     path('apple-touch-icon.png', RedirectView.as_view(url='/static/img/pwa-icon-512.png')),
     path('apple-touch-icon-precomposed.png', RedirectView.as_view(url='/static/img/pwa-icon-512.png')),
+    path('blog/', include('blog.urls')),
     path('', include('expenses.urls')),
 ]
