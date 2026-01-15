16b29dcddb9fde497674d9f881e78f1d02aa032a
Omkar Pathak <omkarpathak27@gmail.com>
2026-01-15 13:28:35 +0530

    Added robots.txt

--- finance_tracker/urls.py
+++ finance_tracker/urls.py
@@ -29,11 +29,28 @@ sitemaps = {
     'blog': BlogSitemap,
 }
 
+from django.http import HttpResponse
+from django.views.decorators.http import require_GET
+
+@require_GET
+def robots_txt(request):
+    lines = [
+        "User-Agent: *",
+        "Allow: /",
+        "Disallow: /admin/",
+        "Disallow: /auth/",
+        "Disallow: /accounts/", 
+        "",
+        f"Sitemap: {request.scheme}://{request.get_host()}/sitemap.xml",
+    ]
+    return HttpResponse("\n".join(lines), content_type="text/plain")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/login/', RedirectView.as_view(pattern_name='account_login', permanent=True)), # Redirect legacy login
     path('accounts/', include('allauth.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt', robots_txt),
     path('favicon.ico', RedirectView.as_view(url='/static/img/pwa-icon-512.png')),
     path('apple-touch-icon.png', RedirectView.as_view(url='/static/img/pwa-icon-512.png')),
     path('apple-touch-icon-precomposed.png', RedirectView.as_view(url='/static/img/pwa-icon-512.png')),
